4d2298c9e0502eba550e1490becdf01637770d95
Za Visual Studio <zavisualstudio2018@outlook.com>
2019-11-29 08:28:16 +0100

    SOLID

---
+++ SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    class Person
+    {
+        public string ime { get; set; }
+        public string prezime { get; set; }
+    }
+}
---
+++ SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobro dosli u aplikaciju!");
+
+            //Trazenje informacija o korisniku
+            Person user = new Person();
+
+            Console.Write("Unesite vase ime: ");
+            user.ime = Console.ReadLine();
+
+            Console.Write("Unesite vase prezime: ");
+            user.prezime = Console.ReadLine();
+
+            //Kreiranje korisnickog imena
+            Console.WriteLine($"Vase korisnicko ime je: {user.ime.ToLower()}.{user.prezime.ToLower()}");
+            Console.ReadLine();
+        }
+    }
+}
